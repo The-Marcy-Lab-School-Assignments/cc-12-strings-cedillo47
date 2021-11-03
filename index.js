14b1847869695c159479df62dbf9a439272d53d2
leo <cedillo47@gmail.com>
2021-11-03 15:03:48 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+function Reverse (String){
+    let word = ""
+ for(let i = String.length-1; 0 <= i; i--){
+     word += String[i]  
+ } 
+ return word
+}
+
+console.log(Reverse("hello"))
